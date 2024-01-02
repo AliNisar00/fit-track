de4ac1b9476518ae83308b40a5e243c0d5a83419
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-01-02 19:46:36 +0500

    Added TabNavigator stylesheet and tab bar background blur (icons not added)

--- navigators/TabNavigator.js
+++ navigators/TabNavigator.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from '../src/screens/HomeScreen'
 import MenuScreen from '../src/screens/MenuScreen'
+import { BlurView } from '@react-native-community/blur'
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +14,10 @@ const TabNavigator = () => {
         screenOptions={{
             headerShown: false,
             tabBarShowLabel: false,
+            tabBarStyle: styles.tabBarStyle,
+            tabBarBackground: () => {
+                <BlurView overlayColor='' blurAmount={15} style={styles.BlurViewStyles} />
+            }
         }}>
         <Tab.Screen name='Home' component={HomeScreen}></Tab.Screen>
         <Tab.Screen name='Menu' component={MenuScreen}></Tab.Screen>
@@ -20,6 +25,22 @@ const TabNavigator = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    tabBarStyle: {
+        height: 80,
+        position: 'absolute',
+        backgroundColor: 'rgba(12,15,20,0.5)', // TO DO: set up a separate color file later
+        borderTopWidth: 0,
+        elevation: 0,
+        borderTopColor: 'transparent',
+    },
+    BlurViewStyles: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+});
 
 export default TabNavigator;
