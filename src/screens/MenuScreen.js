6a4954b66aa4c2df5e776b654c8788cd6877362e
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-01-04 22:22:49 +0500

    react-native-svg version changed to 12.1.0; build succeeded but cannot generate ProgressCircle

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { ProgressCircle } from 'react-native-svg-charts'
 
 const MenuScreen = () => {
   return (
     <View>
       <Text>MenuScreen</Text>
+      <ProgressCircle style={styles.progressCircle} progress={0.7} progressColor={'rgb(134, 65, 244)'} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  progressCircle: {
+    height: 200,
+    marginVertical: 16,
+  },
+});
 
 export default MenuScreen;
