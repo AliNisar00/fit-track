880d60b6aacdfde6b75c241a9ae225e632738eee
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-01-05 19:22:47 +0500

    Modified Android folder files to include blur dependencies; build not successfull yet

--- Components/CardLarge.js
+++ Components/CardLarge.js
@@ -1,19 +1,60 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { Avatar, Button, Card, Text, Title, Subheading  } from "react-native-paper";
+import { BlurView } from "@react-native-community/blur";
 
-const CardLarge = () => (
-    <Card>
-      <Card.Title title="Card Title" subtitle="Card Subtitle"/>
-      <Card.Content>
-        <Text variant="titleLarge">Card title</Text>
-        <Text variant="bodyMedium">Card content</Text>
-      </Card.Content>
-      <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-      <Card.Actions>
-        <Button>Cancel</Button>
-        <Button>Ok</Button>
-      </Card.Actions>
-    </Card>
-);
+const CardLarge = () => {
+    return (
+        <View style={styles.container}>
+          <BlurView
+            style={styles.blurContainer}
+            blurType="light"
+            blurAmount={10}
+            reducedTransparencyFallbackColor="white"
+          />
+          <Card style={styles.card}>
+            <Card.Title title="Card Title" subtitle="Card Subtitle" />
+            <Card.Content>
+              <Text style={styles.title}>Card title</Text>
+              <Text style={styles.content}>Card content</Text>
+            </Card.Content>
+            <Card.Cover source={{ uri: 'https://picsum.photos/700' }} style={styles.cover} />
+            <Card.Actions>
+              <Button style={styles.button}>Cancel</Button>
+              <Button style={styles.button}>Ok</Button>
+            </Card.Actions>
+          </Card>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+      },
+    blurContainer: {
+        ...StyleSheet.absoluteFill,
+    },
+    card: {
+        margin: 10,
+        borderRadius: 10,
+        elevation: 5,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 5,
+    },
+    content: {
+        fontSize: 16,
+        marginBottom: 10,
+    },
+    cover: {
+        height: 200,
+    },
+    button: {
+        marginRight: 10,
+    },
+});
 
 export default CardLarge;
