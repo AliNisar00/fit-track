c0f655783ca1c0ee38a541b19e8978fb9aa5c478
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-01-05 15:21:45 +0500

    Replaced library react-native-svg-charts with react-native-chart-kit for first line chart; Working bezier line chart added in HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,50 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Button, Title, Subheading } from "react-native-paper";
-import { ProgressCircle } from 'react-native-svg-charts'
+import { LineChart } from 'react-native-chart-kit';
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 44],
+        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // Purple color
+        strokeWidth: 2,
+      },
+    ],
+};
 
 const HomeScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <ProgressCircle style={styles.progressCircle} progress={0.7} progressColor={'rgb(134, 65, 244)'} />
+            <Text style={styles.subtitle}>Bezier Line Chart</Text>
+            <LineChart
+                data={data}
+                width={350}
+                height={220}
+                yAxisLabel="$"
+                chartConfig={{
+                backgroundColor: '#e26a00',
+                backgroundGradientFrom: '#fb8c00',
+                backgroundGradientTo: '#ffa726',
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                    borderRadius: 16,
+                },
+                propsForDots: {
+                    r: '6',
+                    strokeWidth: '2',
+                    stroke: '#ffa726',
+                },
+                }}
+                bezier
+                style={{
+                marginVertical: 8,
+                borderRadius: 16,
+                }}
+            />
         </View>
     );
 };
@@ -32,15 +70,12 @@ const styles = StyleSheet.create({
         color: 'white',
     },
     subtitle: {
+        color: 'white',
         fontSize: 18,
         marginBottom: 32,
     },
     button: {
         marginVertical: 8,
-    },
-    progressCircle: {
-        height: 200,
-        marginVertical: 16,
     },
 });
 
--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,21 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { ProgressCircle } from 'react-native-svg-charts'
 
 const MenuScreen = () => {
   return (
     <View>
       <Text>MenuScreen</Text>
-      <ProgressCircle style={styles.progressCircle} progress={0.7} progressColor={'rgb(134, 65, 244)'} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  progressCircle: {
-    height: 200,
-    marginVertical: 16,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default MenuScreen;
