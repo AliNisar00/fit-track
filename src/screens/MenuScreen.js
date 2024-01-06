2197416dd2d317b7e1d668efae97df07a0e85bf6
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-01-06 12:39:25 +0500

    Added async storage sample in MenuScreen

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 const MenuScreen = () => {
+  const [value, setValue] = useState('value');
+  const { getItem, setItem } = useAsyncStorage('@storage_key');
+
+  const readItemFromStorage = async () => {
+    const item = await getItem();
+    setValue(item);
+  };
+
+  const writeItemToStorage = async (newValue) => {
+    await setItem(newValue);
+    setValue(newValue);
+  };
+
+  useEffect(() => {
+    readItemFromStorage();
+  }, []);
+
   return (
-    <View>
-      <Text>MenuScreen</Text>
+    <View style={styles.container}>
+      <Text>Current value: {value}</Text>
+      <TouchableOpacity
+        onPress={() =>
+          writeItemToStorage(
+            Math.random()
+              .toString(36)
+              .substr(2, 5)
+          )
+        }
+      >
+        <Text>Update value</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    margin: 40,
+  },
+});
 
 export default MenuScreen;
