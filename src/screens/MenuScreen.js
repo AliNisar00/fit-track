4c48a7bb23352008f6af31c1c17628f9651de1c0
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-01-07 22:24:22 +0500

    Added step count using pedometer from expo-sensors library in MenuScreen

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,14 +1,53 @@
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pedometer } from 'expo-sensors';
 
 const MenuScreen = () => {
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
+  const [pastStepCount, setPastStepCount] = useState(0);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+
+  const subscribe = async () => {
+    const isAvailable = await Pedometer.isAvailableAsync();
+    setIsPedometerAvailable(String(isAvailable));
+
+    if (isAvailable) {
+      const end = new Date();
+      const start = new Date();
+      start.setDate(end.getDate() - 1);
+
+      const pastStepCountResult = await Pedometer.getStepCountAsync(start, end);
+      if (pastStepCountResult) {
+        setPastStepCount(pastStepCountResult.steps);
+      }
+
+      return Pedometer.watchStepCount(result => {
+        setCurrentStepCount(result.steps);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const subscription = subscribe();
+    return () => subscription && subscription.remove();
+  }, []);
+
   return (
-    <View>
-      <Text>MenuScreen</Text>
+    <View style={styles.container}>
+      <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
+      <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
+      <Text>Walk! And watch this go up: {currentStepCount}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default MenuScreen;
